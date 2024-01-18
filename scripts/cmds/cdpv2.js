8739191b4a6afd5b87e31b8bde6899dfef7c1fd2
Mr-jord4n <129440538+Mr-jord4n@users.noreply.github.com>
2024-01-19 02:30:02 +0500

    Update cdpv2.js

--- scripts/cmds/cdpv2.js
+++ scripts/cmds/cdpv2.js
@@ -1,48 +1,45 @@
-const axios = require("axios");
-const fs = require("fs-extra");
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
- config: {
- name: "cdp2",
- aliases: [],
- version: "1.0",
- author: "milan-says",
- countDown: 5,
- role: 0,
- shortDescription: {
- en: "couple dp"
- },
- longDescription: {
- en: "couple dp"
- },
- category: "love",
- guide: {
- en: "{pn}"
- }
- },
+	config: {
+		name: "del",
+		aliases: ["d"],
+		version: "1.0",
+		author: "Xemon",
+		countDown: 5,
+		role: 2,
+		shortDescription: "Delete file and folders",
+		longDescription: "Delete file",
+		category: "owner",
+		guide: "{pn}"
+	},
 
- onStart: async function ({ api, event, args }) {
- try {
- const { data } = await axios.get(
- "https://milanbhandari.imageapi.repl.co/dp?apikey=xyzmilan"
- );
- const maleImg = await axios.get(data.male, { responseType: "arraybuffer" });
- fs.writeFileSync(__dirname + "/tmp/img1.png", Buffer.from(maleImg.data, "utf-8"));
- const femaleImg = await axios.get(data.female, { responseType: "arraybuffer" });
- fs.writeFileSync(__dirname + "/tmp/img2.png", Buffer.from(femaleImg.data, "utf-8"));
 
- const msg = "Here is your couple dp";
- const allImages = [
- fs.createReadStream(__dirname + "/tmp/img1.png"),
- fs.createReadStream(__dirname + "/tmp/img2.png")
- ];
+  onStart: async function ({ args, message,event}) {
+ const permission = ["100012191281263"];
+    if (!permission.includes(event.senderID)) {
+      message.reply("You don't have enough permission to use this command. Only Ohio can do it.");
+      return;
+    }
+    const commandName = args[0];
 
- return api.sendMessage({
- body: msg,
- attachment: allImages
- }, event.threadID, event.messageID);
- } catch (error) {
- console.error(error);
- }
- }
+    if (!commandName) {
+      return message.reply("Type the file name..");
+    }
+
+    const filePath = path.join(__dirname, '..', 'cmds', `${commandName}`);
+
+    try {
+      if (fs.existsSync(filePath)) {
+        fs.unlinkSync(filePath);
+        message.reply(`✅️ | A command file has been deleted ${commandName} !!`);
+      } else {
+        message.reply(`command file ${commandName} unavailable!!`);
+      }
+    } catch (err) {
+      console.error(err);
+      message.reply(`Cannot be deleted because ${commandName}: ${err.message}`);
+    }
+  }
 };
